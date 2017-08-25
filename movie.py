9fd04a9a7267da137b3d6b821bc22307e1f954c3
willzarem <willmer@osigu.com>
2017-08-24 23:32:34 -0600

    Added comments

--- movie.py
+++ movie.py
@@ -1,11 +1,16 @@
 import webbrowser
 
-class Movie():
-    def __init__(self, title, storyline, poster_image_url, trailer_youtube_url):
+
+# A class that creates a moview from it's title, storyline, poster and trailer
+class Movie:
+    # The constructor of the object with it's params
+    def __init__(self, title, storyline, poster_image_url,
+                 trailer_youtube_url):
         self.title = title
-        self.storyline = storyline	
+        self.storyline = storyline
         self.poster_image_url = poster_image_url
         self.trailer_youtube_url = trailer_youtube_url
 
+    # A function that will open up a browser window and play the movie trailer
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
